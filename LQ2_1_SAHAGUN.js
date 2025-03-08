12d8ffbf40c589ce82778df3d0424193839bd467
sahagunprincess206 <sahagunprincess206@gmail.com>
2025-03-08 10:05:59 +0800

    Create LQ2_1_SAHAGUN.js

---
+++ LQ2_1_SAHAGUN.js
@@ -0,0 +1,8 @@
+let dog_breed = prompt("Enter Dog breed:"
+); //Breed
+let print_count = parseInt(prompt( "Enter
+print count:"));
+
+for (let i = 0; i < print_count; i++) {
+console. log(dog_breed);
+}
